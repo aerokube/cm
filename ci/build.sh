251cae4d903daafc279be660d14cd4e50f360582
Ivan Krutov <vania-pooh@aerokube.com>
2023-03-04 17:37:46 +0300

    Turn off CGO for all platforms in gox (fixes #325)

--- ci/build.sh
+++ ci/build.sh
@@ -3,4 +3,4 @@
 export GO111MODULE="on"
 go install github.com/mitchellh/gox@latest # cross compile
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-X github.com/aerokube/cm/cmd.buildStamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X github.com/aerokube/cm/cmd.gitRevision=`git describe --tags || git rev-parse HEAD` -s -w"
-gox -os "linux darwin windows" -arch "amd64"  -osarch="darwin/arm64" -osarch="windows/386" -output "dist/{{.Dir}}_{{.OS}}_{{.Arch}}" -ldflags "-X github.com/aerokube/cm/cmd.buildStamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X github.com/aerokube/cm/cmd.gitRevision=`git describe --tags || git rev-parse HEAD` -s -w"
+CGO_ENABLED=0 gox -os "linux darwin windows" -arch "amd64"  -osarch="darwin/arm64" -osarch="windows/386" -output "dist/{{.Dir}}_{{.OS}}_{{.Arch}}" -ldflags "-X github.com/aerokube/cm/cmd.buildStamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X github.com/aerokube/cm/cmd.gitRevision=`git describe --tags || git rev-parse HEAD` -s -w"
